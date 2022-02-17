f028a0adb6fd34a9f1ea2a47fc6f8cef88d95d05
Rafał Raźny <rafal-razny@wp.pl>
2022-02-17 16:26:28 +0100

    Update app with json

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from crypt import methods
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def homepage():
-    movies = []
+    response = requests.get("https://api.themoviedb.org/3/movie/550?api_key=3714c210af4bfc6eb0cf27b8bf827c79")
+    movies = response.json()
     return render_template("homepage.html", movies=movies)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
