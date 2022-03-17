a18a063aa1b55b054667d041f8374ab6bbc27acf
Rafał Raźny <rafal-razny@wp.pl>
2022-03-17 22:44:46 +0100

    Add new functions

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def homepage():
     selected_list = request.args.get("list_type", "popular")
     movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
-    list_types = ("popular", "top_rated", "upcoming", "now_playing", "latest")
-    return render_template("homepage.html", movies=movies, current_list=selected_list, list_types=list_types)
+    list_types = ("popular", "top_rated", "upcoming", "now_playing")
+    return render_template("homepage.html", movies=movies, list_types=list_types)
 
-def get_movie_info():
+#def get_movie_info():
     movies_2 = tmdb_client.get_popular_movies()["results"][:8]
     results = {}
     for titles_dict in movies_2:
@@ -38,11 +38,19 @@ def utility_processor():
         return tmdb_client.get_poster_url(path, size)
     return {"cast_image_url": cast_image_url}
 
+@app.context_processor
+def utility_processor():
+    def picture_random_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"picture_random_url": picture_random_url}
+
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
     details = tmdb_client.get_single_movie(movie_id)
     dict_raw = tmdb_client.get_single_movie_cast(movie_id)["cast"][:8]
-    return render_template("movie_details.html", movie=details, casts=dict_raw)
+    collection_photos = tmdb_client.get_random_picture(movie_id)["backdrops"]
+    some_photo = random.choice(collection_photos)
+    return render_template("movie_details.html", movie=details, casts=dict_raw, photos=some_photo)
 
 if __name__ == '__main__':
     app.run(debug=True)
